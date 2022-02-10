5c736185fca12c93b44eb62a64746ec22f5de495
vamsi <vamsi@vamsi.vamsi>
2022-02-10 13:25:59 -0500

    password added

---
+++ passwd.sh
@@ -0,0 +1,12 @@
+echo "Enter the  ppasswoe=rd /:"
+read passwd
+echo "entrt the user Namme:"
+read userid
+
+if [ (( $userid ==  "admin" ) && ( $passwd == "secret" )) ]
+
+then 
+      echo " he is valid user"
+else
+      echo "INvalid user"
+fi
